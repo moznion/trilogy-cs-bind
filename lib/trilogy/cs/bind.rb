44924dad8f884559549633167c6ebb103e964c65
moznion <moznion@mail.moznion.net>
2024-12-07 11:40:59 +0900

    Initial commit
    Signed-off-by: moznion <moznion@mail.moznion.net>

---
+++ lib/trilogy/cs/bind.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "bind/version"
+
+module Trilogy
+  module Cs
+    module Bind
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/trilogy/cs/bind/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Trilogy
+  module Cs
+    module Bind
+      VERSION = "0.1.0"
+    end
+  end
+end
